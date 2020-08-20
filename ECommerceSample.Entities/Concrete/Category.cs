8348073076a3a67e61884cf0e5f653636f4ebf38
Tolga Çakır <cakrtolga13@gmail.com>
2020-08-21 00:55:03 +0300

    Entities were implemented

---
+++ ECommerceSample.Entities/Concrete/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSample.Entities.Concrete
+{
+    public class Category
+    {
+        public short Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Picture { get; set; }
+    }
+}
---
+++ ECommerceSample.Entities/Concrete/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSample.Entities.Concrete
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public short CategoryId { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+    }
+}
