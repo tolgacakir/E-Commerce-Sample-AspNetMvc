54bb9c0e0a1a3c6e7f8cc99d37d9bc58b73670e7
Tolga Çakır <cakrtolga13@gmail.com>
2020-08-21 01:45:44 +0300

    BusinessLogicLayer was implemented.

---
+++ ECommerceSample.BusinessLogicLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using ECommerceSample.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSample.BusinessLogicLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(Category category);
+    }
+}
---
+++ ECommerceSample.BusinessLogicLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,39 @@
+﻿using ECommerceSample.BusinessLogicLayer.Abstract;
+using ECommerceSample.DataAccessLayer.Abstract;
+using ECommerceSample.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerceSample.BusinessLogicLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        private readonly ICategoryDal _categoryDal;
+
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        public void Add(Category category)
+        {
+            _categoryDal.Add(category);
+        }
+
+        public void Delete(Category category)
+        {
+            _categoryDal.Delete(category);
+        }
+
+        public List<Category> GetAll()
+        {
+            return _categoryDal.GetList();
+        }
+
+        public void Update(Category category)
+        {
+            _categoryDal.Update(category);
+        }
+    }
+}
